464fcab9e00b0a07148e5c4435be84ca95c1bfdc
GRODNO\Pavel_Zhurman <pavel_zhurman@epam.com>
2021-05-29 19:41:54 +0300

    added fun giveCookie

--- app/src/main/java/by/epam/testrepository/FirstClass.kt
+++
@@ -1,9 +0,0 @@
-package by.epam.testrepository
-
-class FirstClass {
-    fun theNeedOfCoffee(){
-        println("I need coffee")
-        println("And then smoke a cigarette")
-        println("and one another commit")
-    }
-}
